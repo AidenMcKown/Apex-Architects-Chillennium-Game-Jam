86303648515cdb8081ba97a0f5123924ecf4335f
JasonS4 <97709845+JasonS4@users.noreply.github.com>
2024-03-02 20:04:02 -0600

    Lightning Strike SFX

--- Assets/Scripts/Environment/Challenges/Lightning/LightningChallenge.cs
+++ Assets/Scripts/Environment/Challenges/Lightning/LightningChallenge.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LightningChallenge : Challenge
@@ -11,6 +12,12 @@ public class LightningChallenge : Challenge
     GameObject lightningWarning2;
     [SerializeField]
     GameObject lightningBolt;
+
+    [Header("Audio Settings")]
+    [SerializeField]
+    List<AudioClip> lightningSounds;
+    [SerializeField]
+    float lightningSoundVolume = 13f;
 
 
     public override void Spawn()
@@ -41,15 +48,13 @@ public class LightningChallenge : Challenge
             count++;
             Destroy(lightningWarningGameObject);
         }
-        print("lightning warning done");
         StartCoroutine(SpawnLightning(playerPosition, lightningBolt));
     }
 
     IEnumerator SpawnLightning(Vector3 playerPosition, GameObject lightningBolt)
     {
-        print("spawn lightning");
-        GameObject lightningGameObject;
-        lightningGameObject = Instantiate(lightningBolt, playerPosition - new Vector3(0, 1, 0), Quaternion.Euler(-46.306f, Random.Range(0, 360), -87.125f));
+        GameObject lightningGameObject = Instantiate(lightningBolt, playerPosition - new Vector3(0, 1, 0), Quaternion.Euler(-46.306f, Random.Range(0, 360), -87.125f));
+        AudioSource.PlayClipAtPoint(lightningSounds[Random.Range(0, lightningSounds.Count)], lightningGameObject.transform.position, lightningSoundVolume);
         yield return new WaitForSeconds(1f);
         Destroy(lightningGameObject);
     }
